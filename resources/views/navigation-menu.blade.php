c2db78e856b41049e755233e86ee4475bfd30f26
AlejandroSonera <asonerabenzal@gmail.com>
2025-05-08 14:10:49 +0200

    08/05/2025

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -121,8 +121,11 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+            <x-responsive-nav-link href="{{ route('showclases') }}" :active="request()->routeIs('showclases')">
+                {{ __('CLASES') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link href="{{ route('tienda.index') }}" :active="request()->routeIs('tienda.index')">
+                {{ __('TIENDA') }}
             </x-responsive-nav-link>
         </div>
 
